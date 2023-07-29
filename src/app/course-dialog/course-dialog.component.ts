31c6ac3a06c454160b30776d10058b486119bfa1
Donatus Brain Okwe <braindona@gmail.com>
2023-07-29 07:22:06 -0500

    Implemented an error message for a local component

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -1,58 +1,75 @@
-import {AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Inject,
+  OnInit,
+  ViewChild,
+  ViewEncapsulation,
+} from "@angular/core";
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
-import {Course} from "../model/course";
-import {FormBuilder, Validators, FormGroup} from "@angular/forms";
-import * as moment from 'moment';
-import {catchError} from 'rxjs/operators';
-import {throwError} from 'rxjs';
-import { CoursesService } from '../services/courses.service';
-import { LoadingService } from '../services/loading.service';
+import { Course } from "../model/course";
+import { FormBuilder, Validators, FormGroup } from "@angular/forms";
+import * as moment from "moment";
+import { catchError } from "rxjs/operators";
+import { throwError } from "rxjs";
+import { CoursesService } from "../services/courses.service";
+import { LoadingService } from "../services/loading.service";
+import { MessagesService } from "../messages/messages.service";
 
 @Component({
-    selector: 'course-dialog',
-    templateUrl: './course-dialog.component.html',
-    styleUrls: ['./course-dialog.component.css']
+  selector: "course-dialog",
+  templateUrl: "./course-dialog.component.html",
+  styleUrls: ["./course-dialog.component.css"],
+  providers: [MessagesService],
 })
 export class CourseDialogComponent implements AfterViewInit {
+  form: FormGroup;
 
-    form: FormGroup;
+  course: Course;
 
-    course:Course;
+  constructor(
+    private fb: FormBuilder,
+    private dialogRef: MatDialogRef<CourseDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) course: Course,
+    private courseService: CoursesService,
+    private loadingService: LoadingService,
+    private messagesService: MessagesService
+  ) {
+    this.course = course;
 
-    constructor(
-        private fb: FormBuilder,
-        private dialogRef: MatDialogRef<CourseDialogComponent>,
-        @Inject(MAT_DIALOG_DATA) course:Course,
-        private courseService: CoursesService,
-        private loadingService: LoadingService) {
+    this.form = fb.group({
+      description: [course.description, Validators.required],
+      category: [course.category, Validators.required],
+      releasedAt: [moment(), Validators.required],
+      longDescription: [course.longDescription, Validators.required],
+    });
+  }
 
-        this.course = course;
+  ngAfterViewInit() {}
 
-        this.form = fb.group({
-            description: [course.description, Validators.required],
-            category: [course.category, Validators.required],
-            releasedAt: [moment(), Validators.required],
-            longDescription: [course.longDescription,Validators.required]
-        });
+  save() {
+    const changes = this.form.value;
 
-    }
+    const saveCourse$ = this.courseService
+      .editCourse(this.course.id, changes)
+      .pipe(
+        catchError((error) => {
+          const message = "Could not update the course";
+          this.messagesService.showError(message);
+          console.log(message, error);
+          return throwError(error);
+        })
+      );
 
-    ngAfterViewInit() {
+    this.loadingService
+      .showLoaderUntilCompleted(saveCourse$)
+      .subscribe((res) => {
+        this.dialogRef.close(res);
+      });
+  }
 
-    }
-
-    save() {
-      const changes = this.form.value;
-
-      const saveCourse$ = this.courseService.editCourse(this.course.id, changes)
-			
-			this.loadingService.showLoaderUntilCompleted(saveCourse$).subscribe((res) => {
-        this.dialogRef.close(res)
-      })
-    }
-
-    close() {
-        this.dialogRef.close();
-    }
-
+  close() {
+    this.dialogRef.close();
+  }
 }
--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,26 +1,25 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Course } from '../model/course';
-import { map, shareReplay } from 'rxjs/operators';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Course } from "../model/course";
+import { map, shareReplay } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class CoursesService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   loadCourses(): Observable<Course[]> {
     return this.http.get<Course[]>(`/api/courses`).pipe(
       map((courses) => courses["payload"]),
       shareReplay()
-    )
+    );
   }
 
   editCourse(courseId: string, changes: Course): Observable<any> {
-    return this.http.put<any>(`/api/courses/${courseId}`, changes).pipe(
-      shareReplay()
-    )
+    return this.http
+      .put<any>(`/api/courses/${courseId}`, changes)
+      .pipe(shareReplay());
   }
 }
